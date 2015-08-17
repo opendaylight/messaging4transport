78cf251a0b1a8b8407889d82578097e6e0594396
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2015-08-17 22:08:42 +0100

    Modifying the yang for dom broker
    Change-Id: I445b56afda4d9d6e078f7187935b74e614e030af
    Signed-off-by: Pradeeban Kathiravelu <kk.pradeeban@gmail.com>

--- impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/messaging4transport/impl/rev141210/Messaging4transportModule.java
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/messaging4transport/impl/rev141210/Messaging4transportModule.java
@@ -1,14 +1,4 @@
-/*
- * Copyright (c) 2015 Pradeeban Kathiravelu  and others.  All rights reserved.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210;
-
-import org.opendaylight.messaging4transport.impl.Messaging4transportProvider;
-
 public class Messaging4transportModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210.AbstractMessaging4transportModule {
     public Messaging4transportModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -25,9 +15,8 @@ public class Messaging4transportModule extends org.opendaylight.yang.gen.v1.urn.
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        Messaging4transportProvider provider = new Messaging4transportProvider();
-        getBrokerDependency().registerProvider(provider);
-        return provider;
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
     }
 
 }
