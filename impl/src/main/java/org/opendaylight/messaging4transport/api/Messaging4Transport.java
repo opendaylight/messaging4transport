a25a90d093826cae00630ea4c4daa5fb1d9a901e
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2015-08-18 00:25:32 +0100

    Adding the Messaging4Transport Interface
    Change-Id: I0a18de69c5a973853220cb77aa6adc75b88f978c
    Signed-off-by: Pradeeban Kathiravelu <kk.pradeeban@gmail.com>

---
+++ impl/src/main/java/org/opendaylight/messaging4transport/api/Messaging4Transport.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015 Pradeeban Kathiravelu and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.messaging4transport.api;
+
+/*
+ * This is a simple dummy interface to allow us to create instances of AMQPConfProvider
+ * via the config subsystem.
+ */
+public interface Messaging4Transport {
+}
---
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/messaging4transport/impl/rev141210/Messaging4TransportModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210;
+public class Messaging4TransportModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210.AbstractMessaging4TransportModule {
+    public Messaging4TransportModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public Messaging4TransportModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210.Messaging4TransportModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
