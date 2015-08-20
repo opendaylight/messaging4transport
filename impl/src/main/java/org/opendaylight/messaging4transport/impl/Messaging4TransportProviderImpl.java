8a2abcae4c0ac164c5bd1c2141ce496ac6938027
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2015-08-20 21:00:47 +0100

    Adding the initial impl for createInstance() of messaging4transport.
    Change-Id: I170f01a66a66de42d795070315bfc4c10389bdf6
    Signed-off-by: Pradeeban Kathiravelu <kk.pradeeban@gmail.com>

--- impl/src/main/java/org/opendaylight/messaging4transport/impl/Messaging4TransportProviderImpl.java
+++ impl/src/main/java/org/opendaylight/messaging4transport/impl/Messaging4TransportProviderImpl.java
@@ -7,32 +7,57 @@
  */
 package org.opendaylight.messaging4transport.impl;
 
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
+import org.opendaylight.controller.sal.core.api.Broker.ProviderSession;
+import org.opendaylight.controller.sal.core.api.Provider;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.rev150105.Messaging4transportService;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Messaging4TransportProviderImpl implements BindingAwareProvider, AutoCloseable {
+import java.util.Collections;
+import java.util.Collection;
+
+public class Messaging4TransportProviderImpl implements Provider, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(Messaging4TransportProviderImpl.class);
     private RpcRegistration<Messaging4transportService> messaging4transportService;
+    private ListenerRegistration<SchemaContextListener> listenerRegistration;
 
     @Override
-    public void onSessionInitiated(ProviderContext session) {
+    public void onSessionInitiated(final ProviderSession session) {
         LOG.info("Messaging4TransportProviderImpl Session Initiated");
         Publisher.publish();
-        messaging4transportService = session.addRpcImplementation(Messaging4transportService.class,
-                new Messaging4TransportImpl());
 
+        final DOMDataBroker domDataBroker = session.getService(DOMDataBroker.class);
+
+//        BrokerFacade.getInstance().setContext(session);
+//        BrokerFacade.getInstance().setDomDataBroker( domDataBroker);
+        final SchemaService schemaService = session.getService(SchemaService.class);
+//        listenerRegistration = schemaService.registerSchemaContextListener(ControllerContext.getInstance());
+//        BrokerFacade.getInstance().setRpcService(session.getService(DOMRpcService.class));
+
+
+//        ControllerContext.getInstance().setSchemas(schemaService.getGlobalContext());
+//        ControllerContext.getInstance().setMountService(session.getService(DOMMountPointService.class));
     }
 
     @Override
     public void close() throws Exception {
-        LOG.info("HelloProvider Closed");
+        LOG.info("Messaging4TransportProviderImpl Closed");
         if (messaging4transportService != null) {
             messaging4transportService.close();
         }
     }
+
+    @Override
+    public Collection<ProviderFunctionality> getProviderFunctionality() {
+        return Collections.emptySet();
+    }
+
 }
--- impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/messaging4transport/impl/rev141210/Messaging4TransportModule.java
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/messaging4transport/impl/rev141210/Messaging4TransportModule.java
@@ -1,4 +1,7 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210;
+
+import org.opendaylight.messaging4transport.impl.Messaging4TransportProviderImpl;
+
 public class Messaging4TransportModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.messaging4transport.impl.rev141210.AbstractMessaging4TransportModule {
     public Messaging4TransportModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -15,8 +18,11 @@ public class Messaging4TransportModule extends org.opendaylight.yang.gen.v1.urn.
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        // TODO:implement
-        throw new java.lang.UnsupportedOperationException();
+        // Create an instance of our provider
+        Messaging4TransportProviderImpl instance = new Messaging4TransportProviderImpl();
+        // Register it with the Broker
+        getDomBrokerDependency().registerProvider(instance);
+        return instance;
     }
 
 }
